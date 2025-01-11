8f76c7eaada0db8035306940f47b81d33770ad34
Shreeraj Landge <shreerajlandge1603@gmail.com>
2025-01-11 18:19:09 +0530

    Added task files for interactive navigation menu

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", () => {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.style.background = "#505050";
+    } else {
+        navbar.style.background = "#505050";
+    }
+});
